f5bb40d44f875289b451f0c10586c146cc5b43a8
andrei c <andr@home.com>
2023-08-12 19:57:22 +0300

    initial commit

---
+++ src/app/ui/main-ui.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-ui',
+  template: `
+    <div class="main-container">
+        Main UI
+    </div>
+  `,
+  styles: [],
+  styleUrls:
+  [
+      './main-ui.scss'
+  ]
+})
+export class MainUiComponent
+{
+
+}
