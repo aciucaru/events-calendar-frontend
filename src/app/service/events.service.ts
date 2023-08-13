3bbe2c429b625ed676b1980e75f3dbf4fe768b99
andrei c <andr@home.com>
2023-08-13 18:39:19 +0300

    added buffer arrays for observables

--- src/app/service/events.service.ts
+++ src/app/service/events.service.ts
@@ -10,13 +10,23 @@ import { OutOfOfficeEvent } from '../model/out-of-office-event';
 })
 export class EventsService
 {
+    /* reusable buffer array for the corresponding BehaviorSubject, so that no new array is created everytime
+    the BehaviorSubject needs to ghange and needs a new array; this array is reused and has a initial capacity
+    of 5 weeks * 5 days/week x 8 hours/day x 4 hosted appointments/hour*/
+    private hostedAppointmentArray: Array<MeetingAppointment> = new Array<MeetingAppointment>(5 * 5 * 8 * 4);
     private hostedAppointmentArrayObservable: BehaviorSubject<Array<MeetingAppointment>>
                             = new BehaviorSubject<Array<MeetingAppointment>>(new Array<MeetingAppointment>());
 
+    /* reusable buffer array for the corresponding BehaviorSubject, this array has a initial capacity
+    of 5 weeks * 5 days/week x 8 hours/day x 8 meeting invitations/hour */
+    private invitationArray: Array<Invitation> = new Array<Invitation>(5 * 5 * 8 * 8)
     private invitationArrayObservable: BehaviorSubject<Array<Invitation>>
                                 = new BehaviorSubject<Array<Invitation>>(new Array<Invitation>());
 
-    private outOfOfficeEvents: BehaviorSubject<Array<OutOfOfficeEvent>>
+    /* reusable buffer array for the corresponding BehaviorSubject, this array has a initial capacity
+    of 5 weeks * 5 days/week x 4 out of office events/day */
+    private outOfOfficeEventArray: Array<OutOfOfficeEvent> = new Array<OutOfOfficeEvent>(5 * 5 * 4);
+    private outOfOfficeEventArrayObservable: BehaviorSubject<Array<OutOfOfficeEvent>>
                                 = new BehaviorSubject<Array<OutOfOfficeEvent>>(new Array<OutOfOfficeEvent>());
 
     constructor(private httpClient: HttpClient) { }
