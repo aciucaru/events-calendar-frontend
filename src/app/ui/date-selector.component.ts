111de441342319df3539b1b910c3f2fe8d985d27
andrei c <andr@home.com>
2023-08-13 21:39:12 +0300

    commented out unnecessary code

--- src/app/ui/date-selector.component.ts
+++ src/app/ui/date-selector.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { EventsService } from '../service/events.service';
+
+import { MeetingAppointment } from '../model/meeting-appointment';
+import { Invitation } from '../model/invitation';
+import { OutOfOfficeEvent } from '../model/out-of-office-event';
 
 @Component({
   selector: 'date-selector',
@@ -10,7 +16,35 @@ import { Component } from '@angular/core';
   styles: [],
   styleUrls: ['./date-selector.component.scss']
 })
-export class DateSelectorComponent
+export class DateSelectorComponent implements OnInit
 {
-    
+    // private hostedAppointmentArray: Array<MeetingAppointment> = new Array<MeetingAppointment>(5 * 5 * 8 * 4);
+    // private invitationArray: Array<Invitation> = new Array<Invitation>(5 * 5 * 8 * 8)
+    // private outOfOfficeEventArray: Array<OutOfOfficeEvent> = new Array<OutOfOfficeEvent>(5 * 5 * 4);
+
+    public constructor(protected eventService: EventsService) {}
+
+    public ngOnInit(): void
+    {
+        // this.eventService.getHostedAppointmentArrayObservable()
+        //                     .subscribe( (appointmentArray: Array<MeetingAppointment>) =>
+        //                         {
+        //                             this.hostedAppointmentArray = appointmentArray;
+        //                         }
+        //                     );
+
+        // this.eventService.getInvitationArrayObservable()
+        //                     .subscribe( (invitationArray: Array<Invitation>) =>
+        //                         {
+        //                             this.invitationArray = invitationArray;
+        //                         }
+        //                     );
+
+        // this.eventService.getOutOfOfficeEventArrayObservable()
+        //                     .subscribe( (outOfOfficeEventArray: Array<OutOfOfficeEvent>) =>
+        //                         {
+        //                             this.outOfOfficeEventArray = outOfOfficeEventArray;
+        //                         }
+        //                     );                          
+    }
 }
