90dc6d1e83d0fc0cdaf4708a2454bd6099c1ab6d
andrei c <andr@home.com>
2023-08-13 18:22:16 +0300

    added observables: appointments, invitation, out-of-office

--- src/app/service/events.service.ts
+++ src/app/service/events.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { MeetingAppointment } from '../model/meeting-appointment';
+import { Invitation } from '../model/invitation';
+import { OutOfOfficeEvent } from '../model/out-of-office-event';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventsService {
+export class EventsService
+{
+    private hostedAppointmentArrayObservable: BehaviorSubject<Array<MeetingAppointment>>
+                            = new BehaviorSubject<Array<MeetingAppointment>>(new Array<MeetingAppointment>());
 
-  constructor() { }
+    private invitationArrayObservable: BehaviorSubject<Array<Invitation>>
+                                = new BehaviorSubject<Array<Invitation>>(new Array<Invitation>());
+
+    private outOfOfficeEvents: BehaviorSubject<Array<OutOfOfficeEvent>>
+                                = new BehaviorSubject<Array<OutOfOfficeEvent>>(new Array<OutOfOfficeEvent>());
+
+    constructor(private httpClient: HttpClient) { }
+
+
 }
