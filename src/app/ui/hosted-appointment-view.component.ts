405eb36ba79858ba0560d477c2d9e243eefbf970
andrei c <andr@home.com>
2023-08-15 17:18:04 +0300

    added properties, constructor and ngOnInit()

--- src/app/ui/hosted-appointment-view.component.ts
+++ src/app/ui/hosted-appointment-view.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DateService } from '../service/date.service';
+
+import { MeetingAppointment } from '../model/meeting-appointment';
+import { DateFilter } from '../model/date-filter';
+import { EventsService } from '../service/events.service';
 
 @Component({
   selector: 'hosted-appointment-view',
@@ -7,9 +13,41 @@ import { Component } from '@angular/core';
       hosted-appointment-view works!
     </p>
   `,
-  styles: []
+  styles: [],
+  styleUrls: ['./hosted-appointment-view.component.scss']
 })
-export class HostedAppointmentViewComponent
+export class HostedAppointmentViewComponent implements OnInit
 {
+    protected dateFilter: DateFilter;
+    private hostedAppointmentArray: Array<MeetingAppointment>;
 
+    public constructor(protected eventService: EventsService, protected dateFilterService: DateService)
+    {
+        this.hostedAppointmentArray = new Array<MeetingAppointment>(5 * 5 * 8 * 4);
+
+        this.dateFilter = {
+            year: new Date().getFullYear(),
+            month: new Date().getMonth(),
+            startDate: new Date(),
+            endDate: new Date()
+        };
+    }
+
+    public ngOnInit(): void
+    {
+        this.dateFilterService
+                .getDateFilterObservable()
+                .subscribe( (dateFilter: DateFilter) =>
+                                {
+                                    this.dateFilter = dateFilter;
+                                }
+                            );
+                 
+        this.eventService.getHostedAppointmentArrayObservable()
+                            .subscribe( (appointmentArray: Array<MeetingAppointment>) =>
+                                {
+                                    this.hostedAppointmentArray = appointmentArray;
+                                }
+                            );
+    }
 }
