7f19dc5f14efd1b939cb6e9f5872823c6448841e
andrei c <andr@home.com>
2023-08-16 22:44:04 +0300

    added test tab-panel and tabs

--- src/app/ui/main-ui.component.ts
+++ src/app/ui/main-ui.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
         <top-toolbar></top-toolbar>
         <!-- <hosted-appointment-view></hosted-appointment-view> -->
         <invitations-view></invitations-view>
+        <tab-panel>
+            <tab-item>A</tab-item>
+            <tab-item>B</tab-item>
+            <tab-item>C</tab-item>
+            <tab-item>D</tab-item>
+        </tab-panel>
     </div>
   `,
   styles: [],
