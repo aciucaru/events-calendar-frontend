143ed04bdca7bcfb3172babb5f0ef48298cae2f0
andrei c <andr@home.com>
2023-08-16 21:08:59 +0300

    added tab-item component

---
+++ src/app/ui/tab-item.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab',
+  template: `
+    <p>
+      tab works!
+    </p>
+  `,
+  styles: [],
+  styleUrls: ['./tab-panel.component.scss']
+})
+export class TabItemComponent
+{
+
+}
