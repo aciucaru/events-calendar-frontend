d34b3989977660488fbc934d8a2a3ff4b094af5e
andrei c <andr@home.com>
2023-08-16 22:44:26 +0300

    added inputs

--- src/app/ui/tab-item.component.ts
+++ src/app/ui/tab-item.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'tab',
+  selector: 'tab-item',
   template: `
-    <p>
-      tab works!
-    </p>
+    <div class="main-container">
+        <div>Tab {{tabName}}</div>
+    </div>
   `,
   styles: [],
   styleUrls: ['./tab-panel.component.scss']
 })
 export class TabItemComponent
 {
-
+    @Input() tabName?: string = "tab";
+    @Input() templateRef!: TemplateRef<any>;
+    @Input() isActive: boolean = false;
 }
