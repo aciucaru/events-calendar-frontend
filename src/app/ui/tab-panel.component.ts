15e51d6ce302a1430e1f25eb5cf20297b1463690
andrei c <andr@home.com>
2023-08-16 22:44:49 +0300

    added template and class code

--- src/app/ui/tab-panel.component.ts
+++ src/app/ui/tab-panel.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { TabItemComponent } from './tab-item.component';
 
 @Component({
   selector: 'tab-panel',
   template: `
     <div class="main-container">
-        <div class="tab-buttons">
-            <div></div>
+        <div class="tab-buttons-container">
+            <button [ngClass]="{active: tab == activeComponent}"
+            *ngFor="let tab of tabArray"
+            (click)="activateTab">
+                {{tab.tabName}}
+            </button>
         </div>
         <div class="tab-content">
-
+            
         </div>
     </div>
   `,
@@ -17,5 +22,19 @@ import { Component } from '@angular/core';
 })
 export class TabPanelComponent
 {
+    @ContentChildren(TabItemComponent) tabArray!: QueryList<TabItemComponent>;
+    activeComponent!: TabItemComponent;
+    activeTabIndex: number = 0;
+
+    // protected ngAfterContentInit()
+    // {
+    //     this.activeTabIndex = 0;
+    // }
+
+    protected activateTab(tabIndex: number)
+    {
+        this.activeTabIndex = tabIndex;
+        console.log(`tab activated: ${tabIndex}`);
+    }
 
 }
