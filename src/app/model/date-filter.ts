428f00e44406d07d6dd1261ff7da33c74be4b8d2
andrei c <andr@home.com>
2023-08-19 20:02:19 +0300

    added classes for events from a single day

--- src/app/model/date-filter.ts
+++ src/app/model/date-filter.ts
@@ -1,3 +1,7 @@
+import { Invitation } from "./invitation";
+import { MeetingAppointment } from "./meeting-appointment";
+import { OutOfOfficeEvent } from "./out-of-office-event";
+
 export interface DateFilter
 {
     year: number;
@@ -9,4 +13,34 @@ export interface SingleWeekInterval
 {
     weekStart: Date; // the first day of week, could be different tha Monday
     weekEnd: Date; // the last day of the week, could be different than Sunday
+}
+
+export class SingleDayHostedAppointments
+{
+    private hostedAppointmentArray: Array<MeetingAppointment>;
+
+    public constructor()
+    {
+        this.hostedAppointmentArray = new Array<MeetingAppointment>(5 * 5 * 8 * 4);
+    }
+}
+
+export class SingleDayInvitations
+{
+    private invitationArray: Array<Invitation>;
+
+    public constructor()
+    {
+        this.invitationArray = new Array<Invitation>(5 * 5 * 8 * 8)
+    }
+}
+
+export class SingleDayOutOfOfficeEvents
+{
+    private outOfOfficeEventArray: Array<OutOfOfficeEvent>;
+
+    public constructor()
+    {
+        this.outOfOfficeEventArray = new Array<OutOfOfficeEvent>(5 * 5 * 4);
+    }
 }
