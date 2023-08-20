571175fdf27927b73b19448472f13862b8242a1d
andrei c <andr@home.com>
2023-08-20 14:28:19 +0300

    updated after moving event model classes to single common file

--- src/app/service/date-filter.service.ts
+++ src/app/service/date-filter.service.ts
@@ -34,7 +34,7 @@ export class DateFilterService
         this.currentWeekIndex = 0; // first week of the month
 
         this.currentWeekObservable
-            = new BehaviorSubject<SingleWeekInterval>({ weekStart: new Date(), weekEnd: new Date() });
+            = new BehaviorSubject<SingleWeekInterval>(new SingleWeekInterval(new Date(), new Date()));
 
         this.daysOfCurrentWeek = new Array<Date>();
         this.daysOfCurrentWeekObservable = new BehaviorSubject<Array<Date>>(new Array<Date>());
@@ -54,14 +54,14 @@ export class DateFilterService
             this.currentWeekIndex = dateFilter.weekIndex;
             this.currentWeekObservable.next(this.weekDateIntervals[this.currentWeekIndex]);
 
-            this.calculateDaysOfCurrentWeek();
+            this.daysOfCurrentWeek = this.currentWeekObservable.getValue().calculateDaysOfCurrentWeek();
             this.daysOfCurrentWeekObservable.next(this.daysOfCurrentWeek);
 
             console.log(`setDateFilter year: ${dateFilter.year}`);
             console.log(`setDateFilter month: ${dateFilter.month}`);
             console.log(`setDateFilter weekIndex: ${dateFilter.weekIndex}`);
-            console.log(`setDateFilter weekStart: ${this.weekDateIntervals[this.currentWeekIndex].weekStart}`
-                        + `weekEnd: ${this.weekDateIntervals[this.currentWeekIndex].weekEnd}`);
+            console.log(`setDateFilter weekStart: ${this.weekDateIntervals[this.currentWeekIndex].getStart()}`
+                        + `weekEnd: ${this.weekDateIntervals[this.currentWeekIndex].getEnd()}`);
         }
     }
 
@@ -111,8 +111,8 @@ export class DateFilterService
     private calculateWeekIntervals(year: number, month: number): void
     {
         const numberOfWeeks = this.getWeekCount(year, month);
-        const weekDatesArray: Array<{weekStart: Date, weekEnd: Date}>
-                                = new Array<{weekStart: Date, weekEnd: Date}>(numberOfWeeks);
+        const weekDatesArray: Array<SingleWeekInterval>
+                                = new Array<SingleWeekInterval>(numberOfWeeks);
 
         const firstDayOfMonth = new Date(year, month - 1, 1); // (year, monthIndex, day)
         const lastDayOfMonth = new Date(year, month, 0); // (year, monthIndex, day)
@@ -124,7 +124,7 @@ export class DateFilterService
         const firstWeekStartDay = new Date(firstDayOfMonth);
         const firstWeekEndDay = new Date(firstWeekStartDay);
         firstWeekEndDay.setDate(firstWeekEndDay.getDate() + (7 - startDayIndexOfFirstWeek)); // first Sunday of that month
-        weekDatesArray[0] = {weekStart: firstWeekStartDay, weekEnd: firstWeekEndDay};
+        weekDatesArray[0] = new SingleWeekInterval(firstWeekStartDay, firstWeekEndDay);
 
         // get start and days of last week, the last week might not have 7 complete days
         let endDayIndexOfLastWeek = lastDayOfMonth.getDay(); // returns 0 to 6 (Sunday to Saturday)
@@ -133,7 +133,7 @@ export class DateFilterService
         const lastWeekStartDay = new Date(lastDayOfMonth);
         lastWeekStartDay.setDate(lastWeekStartDay.getDate() - endDayIndexOfLastWeek + 1);
         const lastWeekEndDay = new Date(lastDayOfMonth);
-        weekDatesArray[numberOfWeeks - 1] = {weekStart: lastWeekStartDay, weekEnd: lastWeekEndDay};
+        weekDatesArray[numberOfWeeks - 1] = new SingleWeekInterval(lastWeekStartDay, lastWeekEndDay);
 
         // determine the start and end dates of the in-between weeks
         // the iteration is being made from 2nd to and N-1 week, because the first and last week are already set
@@ -148,36 +148,11 @@ export class DateFilterService
             currentEndDay = new Date(currentStartDay);
             currentEndDay.setDate(currentEndDay.getDate() + 6);
 
-            weekDatesArray[i] = {weekStart: currentStartDay, weekEnd: currentEndDay};
+            weekDatesArray[i] = new SingleWeekInterval(currentStartDay, currentEndDay);
         }
 
         // console.table(weekDatesArray);
 
         this.weekDateIntervals = weekDatesArray;
-    }
-
-    private calculateDaysOfCurrentWeek(): void
-    {
-        const startDaysOfCurrentWeek: Array<Date> = new Array<Date>(this.weekDateIntervals.length);
-        const currentWeek: SingleWeekInterval = this.weekDateIntervals[this.currentWeekIndex];
-
-        const numberOfDaysInCurrentWeek
-                = 1 + Math.round((currentWeek.weekEnd.getTime() - currentWeek.weekStart.getTime()) / (1000 * 3600 * 24));
-        // console.log(`number of days in week: ${numberOfDaysInCurrentWeek}`);
-
-        const firstDayOfCurrentWeek = currentWeek.weekStart;
-        let currentWeekFirstDay: Date = new Date(firstDayOfCurrentWeek);
-
-        for(let i=0; i<numberOfDaysInCurrentWeek; i++)
-        {
-            currentWeekFirstDay = new Date(firstDayOfCurrentWeek);
-            currentWeekFirstDay.setDate(firstDayOfCurrentWeek.getDate() + i); // calculate the first day of current week 
-
-            startDaysOfCurrentWeek[i] = currentWeekFirstDay; // assign first day to arrays of dates
-        }
-
-        // console.log(startDaysOfCurrentWeek);
-        
-        this.daysOfCurrentWeek = startDaysOfCurrentWeek;
     }
 }
