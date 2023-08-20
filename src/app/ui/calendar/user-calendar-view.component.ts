e44462a651b24812e39ee5547019cf8ea6cf0e57
andrei c <andr@home.com>
2023-08-20 19:31:00 +0300

    added service, constructor and week property

--- src/app/ui/calendar/user-calendar-view.component.ts
+++ src/app/ui/calendar/user-calendar-view.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MeetingAppointment } from '../../model/events';
 import { Invitation } from '../../model/events';
 import { EventsService } from '../../service/events.service';
+import { DateFilterService } from 'src/app/service/date-filter.service';
+import { SingleWeekInterval } from 'src/app/model/date-filter';
 
 @Component({
   selector: 'user-calendar-view',
@@ -17,10 +19,22 @@ import { EventsService } from '../../service/events.service';
   styles: [],
   styleUrls: ['./user-calendar-view.component.scss']
 })
-export class UserCalendarViewComponent
+export class UserCalendarViewComponent implements OnInit
 {
-    public constructor()
+    protected currentWeekInterval: SingleWeekInterval;
+
+    public constructor(protected dateFilterService: DateFilterService)
     {
-        
+        this.currentWeekInterval = new SingleWeekInterval(new Date(), new Date());
+    }
+
+    public ngOnInit(): void
+    {
+        this.dateFilterService.getCurrentWeekObservable()
+                                .subscribe( (weekInterval: SingleWeekInterval) =>
+                                    {
+                                        this.currentWeekInterval = weekInterval;
+                                    }
+                                );
     }
 }
